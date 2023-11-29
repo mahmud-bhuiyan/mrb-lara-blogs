86b4e97aaf19976c2a99bc3f815341b76d84bd32
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-11-29 22:18:58 +0600

    admin login system added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Admin\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +23,22 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/admin/login', [AuthenticatedSessionController::class, 'create'])->name('admin.login')->middleware('guest:admin');
+
+Route::post('/admin/login/store', [AuthenticatedSessionController::class, 'store'])->name('admin.login.store');
+
+Route::group(['middleware' => 'admin'], function () {
+
+    Route::get('/admin', [HomeController::class, 'index'])->name('admin.dashboard');
+
+    Route::post('/admin/logout', [AuthenticatedSessionController::class, 'destroy'])->name('admin.logout');
+});
